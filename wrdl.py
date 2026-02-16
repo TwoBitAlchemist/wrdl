7db0a7a1f1f63e9f9042caf140d5f29aee1382a5
Kevin Bay <kevin.bay@samsara.com>
2026-02-15 22:47:39 -0600

    Initial commit

---
+++ wrdl.py
@@ -0,0 +1,155 @@
+import collections
+import random
+
+
+class Wrdl:
+    ANSI_GREEN = "\033[0;32m"
+    ANSI_YELLOW = "\033[0;33m"
+    ANSI_WHITE = "\033[1;37m"
+    ANSI_BOLD = "\033[1m"
+    ANSI_RED = "\033[0;31m"
+
+    class AlreadyGuessed(ValueError):
+        pass
+
+    class InvalidGuess(ValueError):
+        pass
+
+    class OutOfGuesses(Exception):
+        pass
+
+    class NoSuchDictionary(OSError):
+        pass
+
+    def __init__(self, length=5, max_guesses=6, force_starting_word=None):
+        length = max(int(length), 1)
+        try:
+            self.read_dictionary(length)
+        except OSError:
+            raise self.NoSuchDictionary(
+                f"No dictionary loaded for {length}-letter words."
+            )
+        if force_starting_word is not None:
+            force_starting_word = force_starting_word.upper()
+            if force_starting_word in self.__dictionary:
+                self.__secret_word = force_starting_word
+            else:
+                self.__secret_word = random.choice(self.__dictionary)
+        else:
+            self.__secret_word = random.choice(self.__dictionary)
+        self.__max_guesses = max(int(max_guesses), 1)
+        self.__valid_guesses = list()
+
+    def draw(self):
+        markers = {-1: f" {self.ANSI_YELLOW}", 0: " ", 1: f" {self.ANSI_GREEN}"}
+        for i, guess in enumerate(self.__valid_guesses):
+            print(self.ANSI_BOLD, end="")
+            for grade, letter in zip(self._evaluate_guess(i), guess):
+                print(f"[{markers[grade]}{letter}{self.ANSI_WHITE} ]", end="")
+            print()
+        for _ in range(self.max_guesses - len(self.__valid_guesses)):
+            print(self.ANSI_BOLD, end="")
+            print("[   ]" * len(self.secret_word))
+
+    def guess(self, word):
+        word = str(word)
+        if len(word) != len(self.secret_word):
+            raise self.InvalidGuess("Wrong length for a guess!")
+        if any(not char.isalpha() for char in word):
+            raise self.InvalidGuess("Guesses must be letters only.")
+        guess = word.upper()
+        if guess not in self.__dictionary:
+            raise self.InvalidGuess("Unrecognized word.")
+        if guess in self.__valid_guesses:
+            raise self.AlreadyGuessed("Already guessed!")
+
+        self.__valid_guesses.append(guess)
+        if not self.solved and len(self.__valid_guesses) >= self.max_guesses:
+            raise self.OutOfGuesses("Better luck next time!")
+        return self.solved
+
+    def play(self, console_mode=True):
+        if console_mode:
+            try:
+                while not self.solved:
+                    self.draw()
+                    try:
+                        self.guess(input(f"Enter a {len(self.secret_word)}-letter guess: "))
+                    except (self.AlreadyGuessed, self.InvalidGuess) as e:
+                        print(e)
+                    except self.OutOfGuesses as e:
+                        self.draw()
+                        print(e)
+                        print(f"Answer: {self.ANSI_BOLD}{self.ANSI_RED}{self.secret_word}")
+                        break
+            except (EOFError, KeyboardInterrupt):
+                print("\nGame ended prematurely. Thanks for playing!")
+            else:
+                if self.solved:
+                    self.draw()
+                    print(f"{self.ANSI_BOLD}{self.win_message}")
+
+    def read_dictionary(self, length, save=True):
+        with open(f"dictionaries/{length}_letter_words.txt") as wordfile:
+            self.__dictionary = list(set(
+                word.upper().strip() for word in wordfile if len(word.strip()) == length
+            ))
+        if save:
+            self.save_dictionary(length)
+
+    def save_dictionary(self, length):
+        if not self.__dictionary:
+            return
+        with open(
+            f"dictionaries/{length}_letter_words.txt", "w"
+        ) as wordfile:
+            for word in sorted(self.__dictionary):
+                print(word, file=wordfile)
+
+    def _evaluate_guess(self, index=-1):
+        letter_counts = collections.Counter(self.secret_word)
+        guess_letter_counts = collections.defaultdict(int)
+        for position, letter in enumerate(self.__valid_guesses[int(index)]):
+            guess_letter_counts[letter] += 1
+            if self.secret_word[position] == letter:
+                yield 1
+            elif letter in self.secret_word:
+                yield -1 if guess_letter_counts[letter] <= letter_counts[letter] else 0
+            else:
+                yield 0
+
+    @property
+    def max_guesses(self):
+        return self.__max_guesses
+
+    @property
+    def secret_word(self):
+        return self.__secret_word
+
+    @property
+    def solved(self):
+        return bool(self.__valid_guesses) and sum(self._evaluate_guess()) == len(
+            self.secret_word
+        )
+
+    @property
+    def valid_guesses(self):
+        return tuple(self.__valid_guesses)
+
+    @property
+    def win_message(self):
+        match len(self.__valid_guesses):
+            case 0:
+                return "Unbelievable!"
+            case 1:
+                return "Genius!"
+            case 2:
+                return "Magnificent!"
+            case 3:
+                return "Impressive."
+            case 4:
+                return "Splendid."
+            case 5:
+                return "Great."
+            case 6:
+                return "Phew..."
