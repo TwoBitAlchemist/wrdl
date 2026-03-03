8539f7ed4f82a08f3f946c8b4420500803f09a16
Kevin Bay <kevin.bay@samsara.com>
2026-03-03 14:13:07 -0600

    I am a Beenius

--- i_am_a_beenius.py
+++ i_am_a_beenius.py
@@ -1,5 +1,7 @@
 # NYT Spelling Bee Auto-Answers
+import os
 
+os.system("clear")
 letters = ""
 while len(letters) != 7 or not all(char.isalpha() for char in letters):
     letters = input("What are today's letters, in any order? (e.g. UOCIRYT)\n").upper()
@@ -15,6 +17,10 @@ with open("wrdllib/dictionary.txt") as dictionary_file:
         if required_letter in word and all((char in letters) for char in word[:-1])
     ]
 
+print("-------------------------------------------------")
+print("Solutions marked with an asterisk(*) are pangrams.")
+outer_letters = "".join(set(letters) - set(required_letter))
+print(f"\nToday's letters: {outer_letters[:3]}[{required_letter}]{outer_letters[3:]}")
 sort_key = lambda word: (len(word), word)
 previous_length = -1
 for word in sorted(solutions, key=sort_key, reverse=True):
@@ -27,7 +33,10 @@ for word in sorted(solutions, key=sort_key, reverse=True):
         print("===================")
         previous_length = length
         line_length = 0
-    print(word.lower(), end="  ")
+    print(word.lower(), end="")
+    if all((letter in word) for letter in letters):
+        print("*", end="")
+    print(" ", end=" ")
     line_length += length + 2
     if line_length > 80:
         print()
